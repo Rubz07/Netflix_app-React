6709fe7538ff9a9cb13f6ead72038829ba506f77
Rubz07 <rubinsiby@gmail.com>
2021-10-12 23:01:43 +0530

    basic netflix design done

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Banner.css";
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="content">
+        <h1 className="titile">Movie name</h1>
+        <div className="banner_buttons">
+          <button className="button">play</button>
+          <button className="button">my list</button>
+        </div>
+        <h1 className="discription">
+          heloooooooooooooooooooooooooooooooooooooooooooooooooooo
+        </h1>
+      </div>
+     <div className="fade_bottom"></div>
+    
+    </div>
+  );
+}
+
+export default Banner;

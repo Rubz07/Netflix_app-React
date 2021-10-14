bca2b090ee23e8de8ba25682c9057bc0743a2a0f
Rubz07 <rubinsiby@gmail.com>
2021-10-14 16:41:20 +0530

    added automatic banner rotation when refresh and movie details while clicking the poster

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React,{useEffect,useState} from "react";
 import {API_KEY,IMG_URL} from "../../Constants/Constants"
 import axios from "../../axios";
+import random from 'random'
 import "./Banner.css";
 function Banner() {
 const [movie, setMovie] = useState()
@@ -8,10 +9,11 @@ const [movie, setMovie] = useState()
 
  useEffect(() => {
 axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((responce)=>{
-  console.log(responce.data.results[0]);
-  setMovie(responce.data.results[0]);
+  //console.log(responce.data.results);
+ let rand=( random.int((0), (responce.data.results.length)))
+ console.log(responce.data.results[rand])
+  setMovie(responce.data.results[rand]);
 })
-   
  }, [])
 
   return (
@@ -19,7 +21,8 @@ axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((responce)
     <div style={{backgroundImage: `url(${movie? IMG_URL +movie.backdrop_path : "" })`}}
      className="banner">
       <div className="content">
-        <h1 className="titile">{movie ? movie.name:""}</h1>
+        <h1 className="titile">{movie ? movie.title: ""}</h1>
+        
         <div className="banner_buttons">
           <button className="button">play</button>
           <button className="button">my list</button>
--- src/Components/Rowpost/RowPost.js
+++ src/Components/Rowpost/RowPost.js
@@ -5,41 +5,48 @@ import {API_KEY, IMG_URL} from "../../Constants/Constants"
 import YouTube from "react-youtube";
 function RowPost(props) 
 {
-
   const [posts,setPosts]=useState([])
   const [urlId,setUrlId]=useState('')
 
 useEffect(() => {
  axios.get(props.url).then((responce)=>{
-   console.log(responce.data);
+   //console.log(responce.data);
    setPosts(responce.data.results)
  }).catch(err=>{
    alert('nerwork Error')
  })
   }, [])
 
+  const shadow = {
+    top: "0px",
+    right: "0px",
+    bottom: "0px",
+    left: "0px",
+    opacity: 0.4
+}
+//style={{backgroundImage: `url(${ IMG_URL + urlId.image })`,...shadow}}
   //Youtube video options
   const opts = {
     height: '390',
-    width: '100%',
+    width: '90%',
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
+      autoplay: 0,
     },
   };
 
-  const HandleMovieTrailer=(id)=>{
-    console.log(id);
+  const HandleMovieTrailer=(id,name,overview,rat,img)=>{
+    
     axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((responce)=>{
       if(responce.data.results.length!==0){
-        setUrlId(responce.data.results[0].key)
+        console.log(responce.data.results[0])
+        setUrlId({result:responce.data.results[0],moviename:name,movieoverview:overview,rating:rat,image:img})
       }
       else{
         console.log("no trailers");
       }
     })
   }
-
   return(
       
     <div className="row">
@@ -47,11 +54,27 @@ useEffect(() => {
         <div className= "posters">
            {
              posts.map((obj)=>
-               <img onClick={()=>HandleMovieTrailer(obj.id)} className={props.isSmall ? "smallPoster" : "poster"} src={`${IMG_URL+obj.backdrop_path}`} alt="posts" /> 
+               <img onClick={()=>HandleMovieTrailer(obj.id,obj.title,obj.overview,obj.vote_average,obj.backdrop_path)} className={props.isSmall ? "smallPoster" : "poster"} src={`${IMG_URL+obj.backdrop_path}`} alt="posts" /> 
           )}
            
         </div>
-       { urlId && <YouTube opts={opts} videoId={urlId} />} 
+          <div className={urlId? "movieTrailerDetails" :" nomovieTrailerDetails"}  >
+            <div className="youtubeTrailer">{ urlId && <YouTube opts={opts} videoId={urlId.result.key} />}</div>
+        <div className="trailerContent">
+        <h1 className="trailertitle">{urlId.moviename}</h1>
+        <div className="Rating">
+          <h3>Rating : <button className="ratingButton">{urlId.rating}</button> </h3>
+        </div>
+        <h1 className="trailerDiscription">
+          {urlId.movieoverview}
+        </h1>
+        <div className="trailer_buttons">
+          <button className="trailerbutton">Watch Movie</button>
+        </div>
+       </div>
+        </div>
+       
+      
     </div>
   );
 }

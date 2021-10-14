15285f7b1fa9e8b9fb6c9538dd797f29ca06af45
Rubz07 <rubinsiby@gmail.com>
2021-10-14 11:29:00 +0530

    netflix basic prototype done added movie trailer while clicking

--- src/Components/Rowpost/RowPost.js
+++ src/Components/Rowpost/RowPost.js
@@ -1,20 +1,44 @@
 import React,{useEffect,useState} from "react";
 import axios from  "../../axios";
 import "./RowPost.css";
-import {IMG_URL} from "../../Constants/Constants"
+import {API_KEY, IMG_URL} from "../../Constants/Constants"
+import YouTube from "react-youtube";
 function RowPost(props) 
 {
 
   const [posts,setPosts]=useState([])
+  const [urlId,setUrlId]=useState('')
 
 useEffect(() => {
  axios.get(props.url).then((responce)=>{
    console.log(responce.data);
    setPosts(responce.data.results)
  }).catch(err=>{
-   //alert('nerwork Error')
+   alert('nerwork Error')
  })
   }, [])
+
+  //Youtube video options
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const HandleMovieTrailer=(id)=>{
+    console.log(id);
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((responce)=>{
+      if(responce.data.results.length!==0){
+        setUrlId(responce.data.results[0].key)
+      }
+      else{
+        console.log("no trailers");
+      }
+    })
+  }
 
   return(
       
@@ -22,12 +46,12 @@ useEffect(() => {
         <h1>{props.title}</h1>
         <div className= "posters">
            {
-             posts.map((obj)=>{
-               return(<img className={props.isSmall ? "smallPoster" : "poster"} src={`${IMG_URL+obj.backdrop_path}`} alt="posts" />)
-
-             })
-           }
+             posts.map((obj)=>
+               <img onClick={()=>HandleMovieTrailer(obj.id)} className={props.isSmall ? "smallPoster" : "poster"} src={`${IMG_URL+obj.backdrop_path}`} alt="posts" /> 
+          )}
+           
         </div>
+       { urlId && <YouTube opts={opts} videoId={urlId} />} 
     </div>
   );
 }
